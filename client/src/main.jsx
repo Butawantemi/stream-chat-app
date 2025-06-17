23f4727da2224eb96df524edb0f468f040cfba7a
Butawantemi <japhetbuta@gmail.com>
2025-06-17 21:41:15 +0300

    signup page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,16 @@ import App from "./App.jsx";
 
 import { BrowserRouter } from "react-router";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
